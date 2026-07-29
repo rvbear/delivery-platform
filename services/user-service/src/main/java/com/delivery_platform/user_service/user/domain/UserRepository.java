8cbda94af7cba4a1402d766f49d0975572a8e97f
rvbear <rvbear2000@gmail.com>
2026-07-29 23:51:15 +0900

    fix: add repository bean

--- services/user-service/src/main/java/com/delivery_platform/user_service/user/domain/UserRepository.java
+++ services/user-service/src/main/java/com/delivery_platform/user_service/user/domain/UserRepository.java
@@ -1,10 +1,7 @@
 package com.delivery_platform.user_service.user.domain;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface UserRepository {
 
     Optional<User> findByUsername(String username);
--- services/user-service/src/main/java/com/delivery_platform/user_service/user/infrastructure/persistence/UserRepositoryImpl.java
+++ services/user-service/src/main/java/com/delivery_platform/user_service/user/infrastructure/persistence/UserRepositoryImpl.java
@@ -3,9 +3,11 @@ package com.delivery_platform.user_service.user.infrastructure.persistence;
 import com.delivery_platform.user_service.user.domain.User;
 import com.delivery_platform.user_service.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
     private final SpringDataUserRepository repository;
